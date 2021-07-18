857acf551e066cc50b0e306932e2095db4a3883d
최씨 <chl8488@naver.com>
2021-07-18 17:04:49 +0900

    django 17

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -3,5 +3,5 @@ from .views import hello_world
 
 app_name= 'accountapp'
 urlpatterns = [
-    path('hello_word/', hello_world, name='hello_word')
+    path('hello_world/', hello_world, name='hello_world')
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
 from .models import *
 
 # Create your views here.
@@ -11,8 +13,11 @@ def hello_world(request):
         new.text = temp
         new.save()
 
-        return render(request, 'accountapp/helloWorld.html',context=
-        {'hello_world_output':new})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
     else:
+        # helloword 의 모든 데이터를 긁어올 수 있음
+        hello_world_list = helloworld.objects.all()
+
         return render(request, 'accountapp/helloWorld.html', context=
-        {'text': 'GET METHOD!!!'})
+        {'hello_world_list':hello_world_list})
