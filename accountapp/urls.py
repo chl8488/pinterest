262d4e0c760ccc43702502a4ac752b3faf52f27f
최씨 <chl8488@naver.com>
2021-07-19 16:17:54 +0900

    django creation view

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import hello_world
+from .views import hello_world,AccountCreateView
 
 app_name= 'accountapp'
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+    path('create_user/',AccountCreateView.as_view(), name='create'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -9,6 +9,7 @@ from .models import *
 def hello_world(request):
     if request.method == 'POST':
         temp = request.POST.get('hello_world_input')
+
         new = helloworld()
         new.text = temp
         new.save()
@@ -21,3 +22,16 @@ def hello_world(request):
 
         return render(request, 'accountapp/helloWorld.html', context=
         {'hello_world_list':hello_world_list})
+
+
+# CRUD 중 CreateView를 통한 회원가입구현
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
