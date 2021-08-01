01d5e71940facf0ce23c213845b6d0bfaacb0eb1
최씨 <chl8488@naver.com>
2021-08-01 16:29:05 +0900

    장고 Medium Editor(게시글의 content 커스텀마이징)

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,8 +1,17 @@
 from django.forms import ModelForm
 from articleapp.models import Article
+from django import forms
+
+from boardapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class':'editable text-left',
+                                                           'style':'height: auto;'}))
+
+    project = forms.ModelChoiceField(queryset=Project.objects.all(),required=False)
+    # 우리가입력하는게 textarea
+    # text-left (부스트랩에 있는 클래스)
     class Meta:
         model = Article
         fields = ['title','image','project','content']
