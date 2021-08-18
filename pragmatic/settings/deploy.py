7a6aa3dca3b56037b18a6f1b383608f00a5ab835
최씨 <chl8488@naver.com>
2021-08-18 16:45:28 +0900

    django 도커 시크릿

--- pragmatic/settings/deploy.py
+++ pragmatic/settings/deploy.py
@@ -1,5 +1,14 @@
 from .base import *
 
+# secret에서 KEY를 불러오도록 하는 함수
+# secret_name을 받아서 해당 이름을 가진 secret을 읽어오라는 함수
+def read_secret(secret_name):
+    file = open('/run/secrets/'+secret_name)# open()을 이용해 파일을 읽을 수 있따
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -12,7 +21,7 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret('Django_Secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -27,7 +36,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django_1',
-        'PASSWORD': 'password123',
+        'PASSWORD': read_secret('MySQL_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
