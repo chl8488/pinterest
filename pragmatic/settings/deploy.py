c7c776a5133547ca20fcc73d7bf57b317977baa4
최씨 <chl8488@naver.com>
2021-09-05 21:23:44 +0900

    as

--- pragmatic/settings/deploy.py
+++ pragmatic/settings/deploy.py
@@ -9,14 +9,6 @@ def read_secret(secret_name):
     file.close()
     return secret
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-# reading .env file
-environ.Env.read_env(
-    env_file=os.path.join(BASE_DIR, '.env')
-)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
